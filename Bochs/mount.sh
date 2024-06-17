cdd4a237297a50bbc3dee111c8bc578d5f9c6c7e
arce <arce.domartic@gmail.com>
2024-06-17 16:25:13 +0800

    调整文件结构

--- Bochs/mount.sh
+++ Bochs/mount.sh
@@ -2,6 +2,6 @@
 #挂载a.img并复制可运行文件
 sudo mount a.img /mnt/img -t vfat -o loop
 sudo rm -rf /mnt/img/loader.bin
-sudo cp ./loader.bin /mnt/img/
+sudo cp ../Src/loader.bin /mnt/img/
 sudo sync
 sudo umount /mnt/img
